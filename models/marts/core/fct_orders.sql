067b917edff1bdd67222257d57b9c89dca48ebb0
Mohseen <mohseen@velocity.in>
2022-03-23 14:24:07 +0000

    Practice - Payments & Orders

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,34 @@
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+order_payment as (
+
+    select
+        order_id,
+        sum(case when status = 'sucess' then amount end) as amount
+    from payments
+    group by 1
+
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(orders_payment.amount,0) as amount
+    from orders
+    left join order_payment using (order_id)
+)
+
+select * from final
